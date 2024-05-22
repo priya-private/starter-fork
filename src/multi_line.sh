df85c4ed9d1eac5368e07f4a768cd1df3f134847
priya kandababu <priyakandababu@192-168-1-126.tpgi.com.au>
2024-05-22 16:17:30 +1000

    art

--- src/multi_line.sh
+++ src/multi_line.sh
@@ -18,6 +18,9 @@ echo "artifact status"
 output=$(buildkite-agent artifact search foo1.txt 2>&1)
 status=$?
 
+echo "output $output"
+echo "status $status"
+
 if [ $status -eq 0 ]; then
   echo "Artifact found"
 else
