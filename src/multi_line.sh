ca75c6533378e8dac4a8c4e8654ea3ddf12f99e7
priya kandababu <priyakandababu@192-168-1-126.tpgi.com.au>
2024-05-22 15:58:39 +1000

    art

--- src/multi_line.sh
+++ src/multi_line.sh
@@ -10,6 +10,13 @@ cd src
 pwd
 ls
 
+echo "artifact status"
 
+echo "$(buildkite-agent artifact search "foo.txt" --step "step1")"
+if buildkite-agent artifact search "foo.txt" --step "step1"; then
+  echo "artifact exists"
+else 
+  echo "artifact doesn't exists"
+fi
 
 echo "Downloaded"
