d735b633665988cfcbbe7fce16af63e21a990305
priya kandababu <priyakandababu@192-168-1-126.tpgi.com.au>
2024-06-27 12:55:11 +1000

    test dynamic

--- src/generate_block.sh
+++ src/generate_block.sh
@@ -6,9 +6,4 @@ set -eu
 
 # add a new command step to run the tests in each test directory
 
-  echo " - key: ci-blocking-job-ssp-launch-eval 
-   block: block step 
-   label: ":robot_face: DO NOT manually unblock. Waiting for external service to unblock"
-   depends_on: []
-   blocked_state: running"
-
+  echo " - block: block step"
