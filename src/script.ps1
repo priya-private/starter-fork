d7390bed8059d938ca43c4e6935f085ded1cae13
Priya Kandababu <priyakandababu@192-168-1-122.tpgi.com.au>
2024-08-09 10:16:42 +1000

    Test ps

---
+++ src/script.ps1
@@ -0,0 +1,6 @@
+
+Write-Host "Hello world"
+Write-Output "Congratulations! You've successfully run your first build on Buildkite! If you have any questions or need help email support@buildkite.com, we'd be happy to help!"
+
+
+
