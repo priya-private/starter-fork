66681ab19b4469ad0c57b187c451f4be1c169c1d
Priya Kandababu <priyakandababu@192-168-1-125.tpgi.com.au>
2024-09-19 13:04:26 +1000

    test concurrent lable:

--- src/lable.sh
+++ src/lable.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-buildkite-agent step update "label" " xyz $BUILDKITE_JOB_ID" --append --step  "BUILDKITE_PARALLEL_JOB_COUNT" --value "$BUILDKITE_PARALLEL_JOB_COUNT"
+buildkite-agent step update "label" " xyz $BUILDKITE_JOB_ID" --append --step  "concurrent$BUILDKITE_PARALLEL_JOB_COUNT" --value "$BUILDKITE_PARALLEL_JOB_COUNT"
