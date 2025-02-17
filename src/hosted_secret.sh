46ed61edf5a98d8d6fdb4148d7b81857cd625b48
Priya Kandababu <priyakandababu@192-168-1-122.tpgi.com.au>
2025-02-17 12:14:36 +1100

    test script

--- src/hosted_secret.sh
+++ src/hosted_secret.sh
@@ -11,7 +11,11 @@ echo "$(buildkite-agent secret get AWS_ACCOUNT_ID)"
 
 SECRET_VAR=$(buildkite-agent secret get API_TOKEN)
 
+export SECRET_VAR=$SECRET_VAR
+
 ACCOUNT_ID=$(buildkite-agent secret get AWS_ACCOUNT_ID)
+
+export ACCOUNT_ID=$ACCOUNT_ID
 
 echo "secret var is $SECRET_VAR"
 echo "account id is $ACCOUNT_ID"
