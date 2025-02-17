b48518c92e6c65c77b343f7300e068ace8b2dce9
Priya Kandababu <priyakandababu@192-168-1-122.tpgi.com.au>
2025-02-17 12:10:48 +1100

    test script

--- src/hosted_secret.sh
+++ src/hosted_secret.sh
@@ -6,10 +6,21 @@ pwd
 
 echo "secret"
 echo "$(buildkite-agent secret get API_TOKEN)"
+
+echo "$(buildkite-agent secret get AWS_ACCOUNT_ID)"
+
 SECRET_VAR=$(buildkite-agent secret get API_TOKEN)
+ACCOUNT_ID= $(buildkite-agent secret get AWS_ACCOUNT_ID)
 
 echo "secret var is $SECRET_VAR"
-if [ -z "$SECRET_VAR ]; then
+echo "account id is $ACCOUNT_ID"
+
+if [ -z "$SECRET_VAR" ]; then
+echo "API_TOKEN is not set"
+exit 1
+fi
+
+if [ -z "$ACCOUNT_ID" ]; then
 echo "API_TOKEN is not set"
 exit 1
 fi
