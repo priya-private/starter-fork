c30479ca132a34062e6560b0f0d494b47f930757
Priya Kandababu <priyakandababu@192-168-1-122.tpgi.com.au>
2025-02-17 12:01:32 +1100

    test script

--- src/hosted_secret.sh
+++ src/hosted_secret.sh
@@ -5,4 +5,9 @@ set -e
 pwd
 
 echo "secret"
-echo "$(buildkite-agent secret get API_KEY)"
+echo "$(buildkite-agent secret get API_TOKEN)"
+
+if [ -z "$(buildkite-agent secret get API_TOKEN)" ]; then
+echo "API_TOKEN is not set"
+exit 1
+fi
